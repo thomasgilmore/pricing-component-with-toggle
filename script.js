99a3ac0d78b8c189fda6a655aeb9cc2c95dacf38
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-11-19 15:59:11 -0500

    working on switch

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var annualMonthlySwitch = document.getElementById('annualMonthlySwitch');
 
+var counter = 0;
+
 var annualMonthlySwitchFunction = () => {
-    console.log("Working");
+    counter++;
+    if (counter % 2 == 0) {
+        
+    } else {
+
+    }
 };
 
 annualMonthlySwitch.addEventListener('click', annualMonthlySwitchFunction);
